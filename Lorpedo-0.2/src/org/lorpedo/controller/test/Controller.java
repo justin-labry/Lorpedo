ae1b7f3929fd041d49a63c4bc55fc606c33922fd
justin <justin.labry@gmail.com>
2013-10-08 14:58:19 +0900

    package test

---
+++ Lorpedo-0.2/src/org/lorpedo/controller/test/Controller.java
@@ -0,0 +1,16 @@
+package org.lorpedo.controller.test;
+
+import java.util.List;
+import java.util.Map;
+
+public class Controller {
+
+	private static Controller controller = new Controller();
+	Map<Long, List<Link>> switches = null;
+
+	private Controller() { }
+
+	public static Controller getInstance() {
+		return controller;
+	}
+}
